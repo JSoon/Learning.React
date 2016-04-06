9dbd8f4676164b24b22d889ad9f3b643ee10da1b
jsoon <serdeemail@gmail.com>
2016-04-06 18:47:42 +0800

    create bundle with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 /**
  * webpack config
  */
+var webpack = require("webpack");
 
 module.exports = {
-    entry: './html/test/react/test',
+    entry: './html/test/react/main.jsx',
     output: {
-        filename: './html/test/react/test-bundle.js'
+        filename: './html/test/react/main-bundle.js'
     },
     module: {
         loaders: [{
@@ -13,8 +14,14 @@ module.exports = {
             exclude: /(node_modules|bower_components)/,
             loader: 'babel', // 'babel-loader' is also a legal name to reference
             query: {
-                presets: ['es2015']
+                presets: ['react', 'es2015']
             }
         }]
+    },
+    externals: {
+        //don't bundle the 'react' npm package with our bundle.js
+        //but get it from a global 'React' variable
+        'react': 'React',
+        'react-dom': 'ReactDOM'
     }
 };
